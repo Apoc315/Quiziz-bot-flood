bc93dded0086eb83c350dfb276d2658473b3a6b2
Apoc315 <burner53324@gmail.com>
2025-12-03 20:54:36 +0000

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,27 @@
+import platform
+import subprocess
+import sys
 
+def clear_screen():
+    """Clear terminal screen"""
+    os_name = platform.system()
+    if os_name == "Windows":
+        subprocess.run('cls', shell=True)
+    else:
+        subprocess.run('clear', shell=True)
+
+def check_python_version():
+    """Check Python version"""
+    if sys.version_info < (3, 7):
+        print("❌ Python 3.7 or higher is required!")
+        sys.exit(1)
+    return True
+
+def get_os_info():
+    """Get operating system information"""
+    return {
+        'system': platform.system(),
+        'release': platform.release(),
+        'version': platform.version(),
+        'machine': platform.machine()
+    }
